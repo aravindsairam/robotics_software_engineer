df5237a51381ac0ca818ff77f9ec467d1aabb7c6
aravindsairam <aravindsairam1995@gmail.com>
2024-09-30 11:24:44 +0200

    second iter

--- module_4_assessment/module_4_assignment/src/wall_following.cpp
+++ module_4_assessment/module_4_assignment/src/wall_following.cpp
@@ -34,15 +34,15 @@ class WallFollowing : public rclcpp::Node
         void anayseLidarData(const sensor_msgs::msg::LaserScan::SharedPtr msg)
         {
             // get 0-20 and 340-360 degrees for the front side of the robot
-            float _minFront_1 = *std::min_element(msg->ranges.begin(), msg->ranges.begin() + 20);
-            float _minFront_2 = *std::min_element(msg->ranges.begin()+340, msg->ranges.begin() + 360);
+            float _frontScanAvg_1 = *std::min_element(msg->ranges.begin(), msg->ranges.begin() + 20);
+            float _frontScanAvg_2 = *std::min_element(msg->ranges.begin()+340, msg->ranges.begin() + 360);
             // get the minimum value of the front side of the robot
-            _minFront = std::min(_minFront_1, _minFront_2);
+            _frontScanAvg = std::min(_frontScanAvg_1, _frontScanAvg_2);
             // get 70-90 degrees for the left side of the robot
-            _minLeft = *std::min_element(msg->ranges.begin() + 70, msg->ranges.begin() + 90);
+            _leftScanAvg = *std::min_element(msg->ranges.begin() + 70, msg->ranges.begin() + 90);
             // get 260-280 degrees for the right side of the robot
-            _minRight = *std::min_element(msg->ranges.begin() + 260, msg->ranges.begin() + 280);
-            RCLCPP_INFO(this->get_logger(), "Front: %f, Left: %f, Right: %f", _minFront, _minLeft, _minRight);
+            _rightScanAvg = *std::min_element(msg->ranges.begin() + 260, msg->ranges.begin() + 280);
+            RCLCPP_INFO(this->get_logger(), "Front: %f, Left: %f, Right: %f", _frontScanAvg, _leftScanAvg, _rightScanAvg);
         }
             
         void determineState()
@@ -50,38 +50,47 @@ class WallFollowing : public rclcpp::Node
             switch (_state)
             {
                 case Direction::MOVING_FORWARD:
-                    if (_minFront > _maxStopDistance && _minLeft > _maxStopDistance && _minRight > _maxStopDistance)
+                    if (_frontScanAvg > _StopDistance && _leftScanAvg > _StopDistance && _rightScanAvg > _StopDistance)
                     {
                         _state = Direction::STOP_MOVING;
                         break;
                     }
                     else
                     {
-                        if (_minFront < _obstacleDistance)
+                        if (_frontScanAvg > _maxFrontDist && _leftScanAvg > _maxLeftDist && _rightScanAvg > _maxRightDist)
                         {
-                            if (_minRight > _minLeft)
+                            _state = Direction::MOVING_RIGHT;
+                        }
+                        else 
+                        {
+                            if (_frontScanAvg < _minFrontDist)
                             {
-                                _state = Direction::MOVING_RIGHT;
+                                RCLCPP_INFO(this->get_logger(), "in _frontScanAvg < _minFrontDist condition");
+                                if (_rightScanAvg > _leftScanAvg)
+                                {
+                                    _state = Direction::MOVING_RIGHT;
+                                }
+                                else
+                                {
+                                    _state = Direction::MOVING_LEFT;
+                                }
                             }
                             else
                             {
-                                _state = Direction::MOVING_LEFT;
+                                RCLCPP_INFO(this->get_logger(), "_frontScanAvg: %f, _minFrontDist: %f", _frontScanAvg, _minFrontDist);
+                                _state = Direction::MOVING_FORWARD;
                             }
-                        }
-                        else
-                        {
-                            _state = Direction::MOVING_FORWARD;
                         }
                         break;
                     }
                 case Direction::MOVING_LEFT:
-                    if (_minFront > _obstacleDistance)
+                    if (_frontScanAvg > _minFrontDist)
                     {
                         _state = Direction::MOVING_FORWARD;
                     }
                     break;
                 case Direction::MOVING_RIGHT:
-                    if (_minFront > _obstacleDistance)
+                    if (_frontScanAvg > _minFrontDist)
                     {
                         _state = Direction::MOVING_FORWARD;
                     }
@@ -107,7 +116,7 @@ class WallFollowing : public rclcpp::Node
                 case Direction::MOVING_RIGHT:
                     _cmdVel.linear.x = 0.0;
                     _cmdVel.angular.z = -_angularVel;
-                    RCLCPP_INFO(this->get_logger(), "turning left");
+                    RCLCPP_INFO(this->get_logger(), "turning right");
                     break;
                 case Direction::STOP_MOVING:
                     _cmdVel.linear.x = 0.0;
@@ -121,16 +130,18 @@ class WallFollowing : public rclcpp::Node
         geometry_msgs::msg::Twist _cmdVel;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmdVelPub_;
         Direction _state;
-        float _minFront, _minLeft, _minRight;
-        float _obstacleDistance = 1.0;
+        float _frontScanAvg, _leftScanAvg, _rightScanAvg;
+
+        float _minFrontDist = 1.0;
+        float _minLeftDist = 1.0;
+        float _minRightDist = 1.0;
+        float _maxFrontDist = 3.5;
+        float _maxLeftDist = 1.0; //1.50;(1 and 1.5 is working)
+        float _maxRightDist = 1.50;
         
-        float _maxStopDistance = 3.5;
+        float _StopDistance = 3.5;
         float _linearVel = 0.5;
         float _angularVel = 0.25;
-
-
-        // float _maxFrontObstacleDistance = 0.5;
-        // float _minRightObstacleDistance = 0.5;
 
         rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidarSub_;
 };
